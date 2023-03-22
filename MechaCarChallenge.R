52b72530ede4a494e3324519f58d6a99c073d191
Brandon Sangster <117690007+P0ps44@users.noreply.github.com>
2023-03-21 20:08:06 -0400

    fix the t-test issues

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -25,7 +25,8 @@ total_summary <- suspension_coil %>% summarize(Mean_PSI=mean(PSI),
                                           Std_Dev_PSI=sd(PSI),
                                           Num_Coil=n(), .groups = 'keep') 
 
-#Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+#Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions to group 
+#each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil’s PSI column.
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
                                                                          Median_PSI=median(PSI),
                                                                          Var_PSI=var(PSI),
@@ -34,14 +35,14 @@ lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mea
 #Deliverable 3
 
 #Use the t.test() function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
-t.test(suspension_coil$PSI,mu=mean(suspension_coil$PSI))
+t.test(suspension_coil$PSI, mu=1500)
 
 # t-test for lot 1
-t.test(subset(suspension_coil$PSI,suspension_coil$Manufacturing_Lot == "Lot1"),mu=mean(suspension_coil$PSI))
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
 
 # t-test for lot 2
-t.test(subset(suspension_coil$PSI,suspension_coil$Manufacturing_Lot == "Lot2"),mu=mean(suspension_coil$PSI))
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
 
 # t-test for lot 3
-t.test(subset(suspension_coil$PSI,suspension_coil$Manufacturing_Lot == "Lot3"),mu=mean(suspension_coil$PSI))
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
 
